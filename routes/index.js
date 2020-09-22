9d5cbe60b371574a0ccab63b23e03a12bb0faef9
김우현 <link@MacBook-Pro.local>
2020-09-22 22:16:09 +0900

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const { route } = require('./users');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/setTitle/:app', (req, res, next) => {
+  res.render('index', { title: req.app });
 });
 
 module.exports = router;
